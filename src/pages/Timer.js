f0542cd884bbbc1cc7d8d3f2350514a1976e1820
Filip Paulů <ing.fenix@seznam.cz>
2020-03-31 00:55:42 +0200

    Update accuracy of timer

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -21,6 +21,7 @@ import {
   playRefereeWhistle,
 } from '../services/sound';
 
+import {useMounted, useInterval} from '../services/hooks';
 import {useDictionary} from '../services/language';
 
 import KeepAwake from 'react-native-keep-awake';
@@ -32,66 +33,58 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 export default function Timer() {
   const dict = useDictionary();
 
-  const refTimer = useRef(setTimeout(() => {}, 0));
-  const [timer, setTimer] = useState<number | null>(null);
-  const [pause, setPause] = useState(false);
-  const [setTime, setSetTime] = useState(0);
-
   const [showModal, setShowModal] = useState(false);
   const [customTime, setCustomTime] = useState('00:10');
 
   const [sound, setSound] = useState(true);
   const [vibration, setVibration] = useState(true);
+  const [run, setRun] = useState(false);
+  const [timerFrom, setTimerFrom] = useState(0);
+  const time = useInterval<number>(
+    run,
+    delay => {
+      const newTime = timerFrom - Math.floor(delay / 1000);
+      // sound and vibration
+      const half = Math.round(timerFrom / 2);
+      if (vibration && timerFrom > 0) {
+        if ([half, 10, 5, 4, 3, 2, 1].includes(newTime)) {
+          Vibration.vibrate(500);
+        }
+      }
+      if (sound) {
+        if (newTime === 0) {
+          playRefereeWhistle();
+        } else if ([5, 3, 1].includes(newTime)) {
+          playTak();
+        } else if ([4, 2].includes(newTime)) {
+          playTik();
+        } else if (newTime === 10) {
+          playTen();
+        } else if (newTime === half && newTime > 0) {
+          playHalf();
+        }
+      }
 
-  const isMounted = useRef(true);
-  useEffect(
-    () => () => {
-      isMounted.current = false;
+      if (newTime < 0) {
+        setRun(false);
+        return 0;
+      } else {
+        return newTime;
+      }
     },
-    [],
+    1000,
+    0,
   );
 
-  const half = Math.round(setTime / 2);
-
-  // set timer
-  useEffect(() => {
-    clearTimeout(refTimer.current);
-    if (timer !== null && timer > 0 && pause === false) {
-      refTimer.current = setTimeout(() => {
-        isMounted.current && setTimer(timer - 1);
-      }, 1000);
-    } else if (timer === 0) {
-      setTimer(null);
-    }
-  }, [timer, pause]);
-
-  // sound and vibration
-  useEffect(() => {
-    if (vibration && setTime > 0) {
-      if ([half, 10, 5, 4, 3, 2, 1].includes(timer)) {
-        Vibration.vibrate(500);
-      }
-    }
-    if (sound && timer !== null) {
-      if (timer === 0) {
-        playRefereeWhistle();
-      } else if ([5, 3, 1].includes(timer)) {
-        playTik();
-      } else if ([4, 2].includes(timer)) {
-        playTak();
-      } else if (timer === 10) {
-        playTen();
-      } else if (timer === half && timer > 0) {
-        playHalf();
-      }
-    }
-  }, [half, setTime, sound, timer, vibration]);
-
-  function handleTimer(time) {
-    clearTimeout(refTimer.current);
-    setPause(false);
-    setSetTime(time);
-    setTimer(time);
+  function handleStartTimer(timer: number) {
+    setRun(true);
+    setTimerFrom(timer);
+  }
+  function handlePause() {
+    setRun(false);
+  }
+  function handleStop() {
+    setRun(false);
   }
 
   return (
@@ -105,7 +98,7 @@ export default function Timer() {
           right: 16,
           flexDirection: 'row',
         }}>
-        <TouchableNativeFeedback onPress={() => setVibration(!vibration)}>
+        <TouchableNativeFeedback onPress={() => setVibration(v => !v)}>
           <Icon
             name={vibration ? 'vibrate' : 'vibrate-off'}
             color="black"
@@ -113,7 +106,7 @@ export default function Timer() {
           />
         </TouchableNativeFeedback>
         <View style={{paddingLeft: 16}} />
-        <TouchableNativeFeedback onPress={() => setSound(!sound)}>
+        <TouchableNativeFeedback onPress={() => setSound(s => !s)}>
           <Icon
             name={sound ? 'volume-high' : 'volume-off'}
             color="black"
@@ -126,30 +119,31 @@ export default function Timer() {
         <View style={{flexGrow: 1, justifyContent: 'center'}}>
           <View style={{flexDirection: 'row'}}>
             <Text style={{fontSize: 20}}>
-              {setTime > 0 && <Time>{setTime}</Time>}
+              {timerFrom > 0 && <Time>{timerFrom}</Time>}
             </Text>
-            {setTime > 0 && timer === null && (
+            {!run && time === 0 && timerFrom > 0 && (
               <View style={{flexDirection: 'row', marginLeft: 16}}>
-                <Button onPress={() => handleTimer(setTime)}>
+                <Button onPress={() => handleStartTimer(timerFrom)}>
                   <Icon name="repeat" />
                 </Button>
                 <View style={{width: 16}} />
-                <Button onPress={() => handleTimer(Math.round(setTime / 2))}>
+                <Button
+                  onPress={() => handleStartTimer(Math.round(timerFrom / 2))}>
                   {dict('halfTime')}
                 </Button>
               </View>
             )}
           </View>
           <Text style={{fontSize: 100, textAlign: 'center'}}>
-            <Time>{timer}</Time>
+            <Time>{time}</Time>
           </Text>
-          {timer != null && (
+          {time > 0 && (
             <View style={{flexDirection: 'row'}}>
-              <Button onPress={() => setPause(!pause)}>
-                <Icon name={pause ? 'play' : 'pause'} />
+              <Button onPress={() => handlePause()}>
+                <Icon name={run ? 'pause' : 'play'} />
               </Button>
               <View style={{width: 16}} />
-              <Button onPress={() => setTimer(null)}>
+              <Button onPress={() => handleStop()}>
                 <Icon name={'stop'} />
               </Button>
               <KeepAwake />
@@ -158,13 +152,13 @@ export default function Timer() {
         </View>
 
         <View style={{flexDirection: 'row'}}>
-          <Button onPress={() => handleTimer(300)}>5:00</Button>
+          <Button onPress={() => handleStartTimer(300)}>5:00</Button>
           <View style={{width: 16}} />
-          <Button onPress={() => handleTimer(120)}>2:00</Button>
+          <Button onPress={() => handleStartTimer(120)}>2:00</Button>
           <View style={{width: 16}} />
-          <Button onPress={() => handleTimer(60)}>1:00</Button>
+          <Button onPress={() => handleStartTimer(60)}>1:00</Button>
           <View style={{width: 16}} />
-          <Button onPress={() => handleTimer(30)}>0:30</Button>
+          <Button onPress={() => handleStartTimer(30)}>0:30</Button>
         </View>
         <View
           style={{flexDirection: 'row', alignItems: 'center', marginTop: 16}}>
@@ -174,7 +168,7 @@ export default function Timer() {
             show={showModal}
             onPress={() => {
               setShowModal(false);
-              handleTimer(getTimeFromString(customTime));
+              handleStartTimer(getTimeFromString(customTime));
             }}
             onClose={() => setShowModal(false)}
             title={dict('setTime')}
@@ -191,7 +185,9 @@ export default function Timer() {
               autoFocus
               keyboardType="numeric"
               value={customTime}
-              onChangeText={time => setCustomTime(getValidCustomTime(time))}
+              onChangeText={timeString =>
+                setCustomTime(getValidCustomTime(timeString))
+              }
             />
           </Modal>
         </View>
@@ -212,7 +208,21 @@ function Time({children}) {
     </>
   );
 }
+/*
+function handleTimer(setTime: number, setTimer: TimerState => TimerState) {
+  setTimer(t => {
+    clearInterval(t.interval);
+    const start = Date.now();
 
+    const interval = setInterval(() => {
+      setTimer(timer => {
+
+    }, 1000);
+
+    return {...t, setTime, interval, run: true, time: setTime};
+  });
+}
+*/
 function getTimeFromString(time: string): number {
   const [min, sec] = time.split(':').map(t => parseInt(t, 10));
   if (typeof sec !== 'number') {
